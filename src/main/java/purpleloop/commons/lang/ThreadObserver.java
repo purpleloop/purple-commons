9133ba048c88c2ea4aa3caba8f9b6573306f7278
purpleloop <38167219+purpleloop@users.noreply.github.com>
2020-07-27 01:02:22 +0200

    + ThreadObserver and adapter

---
+++ src/main/java/purpleloop/commons/lang/ThreadObserver.java
@@ -0,0 +1,28 @@
+package purpleloop.commons.lang;
+
+/**
+ * This interface is useful when controlling a thread.
+ * 
+ * <p>
+ * The thread owner, for instance, registers as a ThreadObserver and can be
+ * notified when the thread requires it.
+ * </p>
+ */
+public interface ThreadObserver {
+
+    /**
+     * Message from the thread to for the observer.
+     * 
+     * @param sourceThread source thread for the notification
+     * @param message message to transmit
+     */
+    void threadMessage(Thread sourceThread, String message);
+
+    /**
+     * Callback function used to notify the death of a thread.
+     * 
+     * @param sourceThread source thread for the notification
+     */
+    void threadDeath(Thread sourceThread);
+
+}
