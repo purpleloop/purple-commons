42671cc8c9acd37ecc51596c97656e4bb886dcda
purpleloop <38167219+purpleloop@users.noreply.github.com>
2025-05-03 02:59:23 +0200

    * Math, add a method to compute the angle of a segment

--- src/main/java/io/github/purpleloop/commons/math/geom/GeomUtils.java
+++ src/main/java/io/github/purpleloop/commons/math/geom/GeomUtils.java
@@ -1,7 +1,7 @@
 package io.github.purpleloop.commons.math.geom;
 
 /** Geometric utilities. */
-public final class GeomUtils {   
+public final class GeomUtils {
 
     /** Private constructor. */
     private GeomUtils() {
@@ -19,6 +19,31 @@ public final class GeomUtils {
      */
     public static double distance(double x1, double y1, double x2, double y2) {
         return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    /**
+     * Given the point M(x,y), computes the angle for the segment OM, with
+     * horizontal axis.
+     * 
+     * @param x
+     * @param y
+     * @return angle of the segment
+     */
+    public static double angleForSegment(double x, double y) {
+        double ang;
+
+        if (x != 0) {
+            ang = Math.atan(y / x);
+        } else if (y < 0) {
+            ang = -Math.PI / 2;
+        } else {
+            ang = Math.PI / 2;
+        }
+
+        if (x < 0) {
+            ang = ang + Math.PI;
+        }
+        return ang;
     }
 
 }
